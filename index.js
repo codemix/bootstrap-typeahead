b960be5ffcc07d5c60a3232b3f799e47af978627
Charles Pick <charles.pick@gmail.com>
2013-03-31 20:17:42 +0100

    Added tag v2.3.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function(){
   var jQuery = require('jquery');
   /* =============================================================
-   * bootstrap-typeahead.js v2.3.0
+   * bootstrap-typeahead.js v2.3.1
    * http://twitter.github.com/bootstrap/javascript.html#typeahead
    * =============================================================
    * Copyright 2012 Twitter, Inc.
