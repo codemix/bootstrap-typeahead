6f09fc4464c874adc23360c6d7a91680ba486ccc
Charles Pick <charles.pick@gmail.com>
2013-03-31 20:17:34 +0100

    Added tag v2.1.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function(){
   var jQuery = require('jquery');
   /* =============================================================
-   * bootstrap-typeahead.js v2.0.4
+   * bootstrap-typeahead.js v2.1.0
    * http://twitter.github.com/bootstrap/javascript.html#typeahead
    * =============================================================
    * Copyright 2012 Twitter, Inc.
@@ -79,17 +79,23 @@ function(){
       }
   
     , lookup: function (event) {
-        var that = this
-          , items
-          , q
+        var items
   
         this.query = this.$element.val()
   
-        if (!this.query) {
+        if (!this.query || this.query.length < this.options.minLength) {
           return this.shown ? this.hide() : this
         }
   
-        items = $.grep(this.source, function (item) {
+        items = $.isFunction(this.source) ? this.source(this.query, $.proxy(this.process, this)) : this.source
+  
+        return items ? this.process(items) : this
+      }
+  
+    , process: function (items) {
+        var that = this
+  
+        items = $.grep(items, function (item) {
           return that.matcher(item)
         })
   
@@ -171,12 +177,46 @@ function(){
           .on('keyup',    $.proxy(this.keyup, this))
   
         if ($.browser.webkit || $.browser.msie) {
-          this.$element.on('keydown', $.proxy(this.keypress, this))
+          this.$element.on('keydown', $.proxy(this.keydown, this))
         }
   
         this.$menu
           .on('click', $.proxy(this.click, this))
           .on('mouseenter', 'li', $.proxy(this.mouseenter, this))
+      }
+  
+    , move: function (e) {
+        if (!this.shown) return
+  
+        switch(e.keyCode) {
+          case 9: // tab
+          case 13: // enter
+          case 27: // escape
+            e.preventDefault()
+            break
+  
+          case 38: // up arrow
+            e.preventDefault()
+            this.prev()
+            break
+  
+          case 40: // down arrow
+            e.preventDefault()
+            this.next()
+            break
+        }
+  
+        e.stopPropagation()
+      }
+  
+    , keydown: function (e) {
+        this.suppressKeyPressRepeat = !~$.inArray(e.keyCode, [40,38,9,13,27])
+        this.move(e)
+      }
+  
+    , keypress: function (e) {
+        if (this.suppressKeyPressRepeat) return
+        this.move(e)
       }
   
     , keyup: function (e) {
@@ -203,32 +243,6 @@ function(){
         e.stopPropagation()
         e.preventDefault()
     }
-  
-    , keypress: function (e) {
-        if (!this.shown) return
-  
-        switch(e.keyCode) {
-          case 9: // tab
-          case 13: // enter
-          case 27: // escape
-            e.preventDefault()
-            break
-  
-          case 38: // up arrow
-            if (e.type != 'keydown') break
-            e.preventDefault()
-            this.prev()
-            break
-  
-          case 40: // down arrow
-            if (e.type != 'keydown') break
-            e.preventDefault()
-            this.next()
-            break
-        }
-  
-        e.stopPropagation()
-      }
   
     , blur: function (e) {
         var that = this
@@ -267,12 +281,13 @@ function(){
     , items: 8
     , menu: '<ul class="typeahead dropdown-menu"></ul>'
     , item: '<li><a href="#"></a></li>'
+    , minLength: 1
     }
   
     $.fn.typeahead.Constructor = Typeahead
   
   
-   /* TYPEAHEAD DATA-API
+   /*   TYPEAHEAD DATA-API
     * ================== */
   
     $(function () {
@@ -285,4 +300,5 @@ function(){
     })
   
   }(jQuery);
+  
 }
